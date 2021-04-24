5626a1f53221f255585ce0944de38bcf7b5daf10
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 20:05:46 +0000

    YouTubeCounterSection.js: changed

--- src/components/YouTubeCounterSection.js
+++ src/components/YouTubeCounterSection.js
@@ -6,29 +6,19 @@ import {toStyleObj, withPrefix, markdownify, Link, classNames} from '../utils';
 export default class YouTubeCounterSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
-        let bg_img_opacity_pct = _.get(section, 'background_image_opacity', null) || 100;
-        let bg_img_opacity = bg_img_opacity_pct * 0.01;
         return (
-            <section className="hero bg-color" data-id={_.get(section, 'section_id', null)}>
-                {_.get(section, 'background_image', null) && (
-                <div className="hero__bg-img" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'background_image', null)) + '\'); opacity: ' + bg_img_opacity + ';')}/>
-                )}
-                <div className="hero__title">{markdownify(_.get(section, 'content', null))}</div>
-                <div className="hero__links link-group">
-                  {_.map(_.get(section, 'actions', null), (action, action_idx) => {
-                      let action_style = _.get(action, 'style', null) || 'primary';
-                      return (
-                        <Link key={action_idx} href={withPrefix(_.get(action, 'url', null))} className={classNames('link', {'link--filled': action_style === 'primary', 'link--borderless': action_style === 'link'})}>
-                          {_.get(action, 'title', null)}
-                          {_.get(action, 'arrow', null) && (
-                          <svg width="26" height="14" viewBox="0 0 26 14" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M22.682 6.079h-22.682v1.712h22.814l-4.574 4.528 1.194 1.182 6.566-6.5-6.566-6.5-1.194 1.182 4.442 4.397z" />
-                          </svg>
-                          )}
-                        </Link>
-                      )
-                  })}
+            <section className="content__row bulletpoints__section" data-id={_.get(section, 'section_id', null)}>
+            {_.map(_.get(section, 'bulletpoints', null), (bulletpoint, bulletpoint_idx) => (
+              <section key={bulletpoint_idx} className="content__row bulletpoint__section">
+                <Picture {...this.props} image={_.get(bulletpoint, 'image', null)} alt={_.get(bulletpoint, 'title', null)} cssClass={'bulletpoint__section-image'} />
+                <div className="bulletpoint__section-content-container">
+                  <h2 className="bulletpoint__section-title">{_.get(bulletpoint, 'title', null)}</h2>
+                  <div className="bulletpoint__section-content">
+                    {markdownify(_.get(bulletpoint, 'description', null))}
+                  </div>
                 </div>
+              </section>
+            ))}
             </section>
         );
     }
