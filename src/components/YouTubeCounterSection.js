35521c4e10d1b01cf22c363591bacc4d2877a5a4
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 20:06:32 +0000

    YouTubeCounterSection.js: changed

--- src/components/YouTubeCounterSection.js
+++ src/components/YouTubeCounterSection.js
@@ -8,17 +8,9 @@ export default class YouTubeCounterSection extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section className="content__row youtubecounter__section" data-id={_.get(section, 'section_id', null)}>
-            {_.map(_.get(section, 'youtubecounter', null), (bulletpoint, bulletpoint_idx) => (
-              <section key={bulletpoint_idx} className="content__row bulletpoint__section">
-                <Picture {...this.props} image={_.get(bulletpoint, 'image', null)} alt={_.get(bulletpoint, 'title', null)} cssClass={'bulletpoint__section-image'} />
-                <div className="bulletpoint__section-content-container">
-                  <h2 className="bulletpoint__section-title">{_.get(bulletpoint, 'title', null)}</h2>
-                  <div className="bulletpoint__section-content">
-                    {markdownify(_.get(bulletpoint, 'description', null))}
-                  </div>
+                <div>
+                    
                 </div>
-              </section>
-            ))}
             </section>
         );
     }
